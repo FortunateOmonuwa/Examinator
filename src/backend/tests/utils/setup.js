8730272b5b9289a889882aefbe3d795b39fadc3b
Fortunate Omonuwa <omonuwafortunate@gmail.com>
2025-06-01 23:59:15 +0100

    update yml

--- src/backend/tests/utils/setup.js
+++ src/backend/tests/utils/setup.js
@@ -7,22 +7,6 @@ import dotenv from "dotenv";
 // Load environment variables for tests
 dotenv.config();
 
-// Set default environment variables for testing if missing
-if (!process.env.JWT_SECRET) {
-  process.env.JWT_SECRET =
-    "d4c1a7c563e69220b108c0a1ef3164c4af998fea96cc23ca7c3b476802ed9ada5605bda7055831eb6ee1c40c7776fe04070bcb999635a143f045ecf35304d32f";
-}
-if (!process.env.REFRESH_TOKEN) {
-  process.env.REFRESH_TOKEN =
-    "8c1c7fbd65c626e9ca0561968d6d827ca8ee52f2f338ec365b3cf93e2ef380c93894a9dec16712f96c203a184e116b4b621aaaad9ee4510f47002c571da4c2a4";
-}
-if (!process.env.PORT) {
-  process.env.PORT = "5000";
-}
-if (!process.env.BASE_URL) {
-  process.env.BASE_URL = "http://localhost:5173";
-}
-
 // Global setup
 beforeAll(async () => {
   // console.log('Setting up test environment');
