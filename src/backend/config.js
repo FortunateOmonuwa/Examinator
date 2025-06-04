7ac0615a5d153c38b12b2717bacddb6c5e45ffd1
Fortunate Omonuwa <omonuwafortunate@gmail.com>
2025-06-04 18:31:56 +0100

    update CI.yml and cors config to allow dev base url

--- src/backend/config.js
+++ src/backend/config.js
@@ -5,7 +5,12 @@ import cors from "cors";
 
 const corsConfig = () => {
   return cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:3000",
+      "https://examinator-frontend-dev.onrender.com",
+      "https://examinator-frontend.onrender.com",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
