aee4d2b40a23109b4cbece39e3c371d309dea7a3
Fortunate Omonuwa <omonuwafortunate@gmail.com>
2025-06-28 02:39:22 +0100

    update redis config

--- src/backend/utilities/Redis.config.js
+++ src/backend/utilities/Redis.config.js
@@ -1,6 +1,6 @@
 // redisClient.ts (or .js if you're not on TS)
 import Redis from "ioredis";
-import { redisHost, redisPort } from "../../../Config";
+import { redisHost, redisPort } from "../../../Config.js";
 const client = new Redis({
   host: redisHost,
   port: redisPort,
