5d929e23520b635b75ca00dca3f07f458aaba3de
Fortunate Omonuwa <omonuwafortunate@gmail.com>
2025-07-07 19:10:17 +0100

    side bar context

---
+++ src/frontend/src/contexts/SidebarContext.jsx
@@ -0,0 +1,35 @@
+import { createContext, useContext, useState } from 'react';
+
+const SidebarContext = createContext();
+
+export const useSidebar = () => {
+  const context = useContext(SidebarContext);
+  if (!context) {
+    throw new Error('useSidebar must be used within a SidebarProvider');
+  }
+  return context;
+};
+
+export const SidebarProvider = ({ children }) => {
+  const [isMobileSidebarOpen, setIsMobileSidebarOpen] = useState(false);
+
+  const toggleMobileSidebar = () => {
+    setIsMobileSidebarOpen(!isMobileSidebarOpen);
+  };
+
+  const closeMobileSidebar = () => {
+    setIsMobileSidebarOpen(false);
+  };
+
+  return (
+    <SidebarContext.Provider
+      value={{
+        isMobileSidebarOpen,
+        toggleMobileSidebar,
+        closeMobileSidebar,
+      }}
+    >
+      {children}
+    </SidebarContext.Provider>
+  );
+};
