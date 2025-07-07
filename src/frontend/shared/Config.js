ded493f70f9aedd5a1f7a469a20db47e17f3ed74
Fortunate Omonuwa <omonuwafortunate@gmail.com>
2025-07-07 10:16:16 +0100

    update config

--- src/frontend/shared/Config.js
+++ src/frontend/shared/Config.js
@@ -1,20 +1,14 @@
 let baseUrl = "";
 let clientUrl = "";
-let redisHost;
-let redisPort;
 //const env = "local";
 const env = "dev";
 //-------------------------------------------------
 if (env === "local") {
   baseUrl = "http://localhost:5001";
   clientUrl = "http://localhost:5173";
-  redisHost = "127.0.0.1";
-  redisPort = 6379;
 } else if (env === "dev") {
   baseUrl = "https://examinator-backend-dev.onrender.com";
   clientUrl = "https://examinatorr.netlify.app";
-  redisHost = process.env.REDIS_HOST;
-  redisPort = process.env.REDIS_PORT;
 }
 
-export { baseUrl, clientUrl, redisHost, redisPort };
+export { baseUrl, clientUrl };
